1fae3e40e7183207efa58637c5d103c1859939c6
skillBoardEdu <skillboardeducationpandikkad@gmail.com>
2024-04-22 12:47:54 +0530

    debug=false

--- skillBoardPrj/settings.py
+++ skillBoardPrj/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)+msqd1z54g6ncq$0i@vk)4^s(eq6*lr(-(xrf!(mav2jxfe!('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["production.eba-ggt7yk5g.us-west-2.elasticbeanstalk.com","staging.eba-ggt7yk5g.us-west-2.elasticbeanstalk.com","skillboardedu.in","www.skillboardedu.in","127.0.0.1","172.31.41.164"]
 
